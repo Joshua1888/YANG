3c0baa4124d5119b43baa9806c06b4ec3bab0d08
Talentjoe <88471172+Talentjoe@users.noreply.github.com>
2024-09-11 22:30:22 -0700

    update

--- Assets/Resources/CardInterface.cs
+++ Assets/Resources/CardInterface.cs
@@ -2,8 +2,8 @@
 //files to storage card interface
 using System.Collections.Generic;
 using System;
-using UnityEngine;
-using UnityEditor;
+using UnityEngine; 
+using UnityEditor; 
 
 [CreateAssetMenu(fileName = "CardInterface", menuName = "CardInterface")]
 public class CardInterface : ScriptableObject
@@ -11,7 +11,7 @@ public class CardInterface : ScriptableObject
     public List<CardInterfaceStorage> cardInterface;
 }
 [Serializable] 
-public class CardInterfaceStorage
+public class CardInterfaceStorage 
 {
     public CardType type;
     public Sprite sprite;
